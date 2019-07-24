90f5dc3392b133ec8520eeb34ee34b4997482298
Gabriel Mello <gabrielmello808@gmail.com>
2019-07-23 22:35:13 -0300

    build

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 git pull
 docker stack rm ollemg
+images=docker ps -a | awk '{print $1}'
+for i in "${images[*]}" do
+docker rm $i;
+done
 docker stack deploy -c docker-compose.yml ollemg
